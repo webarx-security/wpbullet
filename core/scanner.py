a1efddb0056906be51d4ee25343699edf4a57fd6
Luka Sikic <luka@sikic.eu>
2019-04-22 18:39:46 +0200

    Skip grouping all rules, run scan from walker

--- core/scanner.py
+++ core/scanner.py
@@ -4,20 +4,24 @@ import os
 
 # Main function to handle files processing
 def scan(args):
-
-    files = scope_files(args.path)
-
     modules = {
         'enabled': [x.strip() for x in args.enabled.split(',')],
         'disabled': [x.strip() for x in args.disabled.split(',')]
     }
 
-    file_no = 0
-    for file in files:
-        file_no = file_no + 1
-        print(str(file_no) + "/" + str(len(files)), end="\r")
-        content = read_file(file)
-        process_file(content, file, modules)
+    count_files = 0
+    for r, d, f in os.walk(args.path):
+        for file in f:
+            if '.php' in file:
+                count_files = count_files + 1
+                print('Checked files: ' + str(count_files), end="\r")
+                check_file(file, r, modules)
+
+
+def check_file(file,r, modules):
+    path = os.path.join(r, file)
+    content = read_file(path)
+    process_file(content, path, modules)
 
 
 # Process single given file, run all enabled modules
@@ -29,17 +33,6 @@ def process_file(content, file, modules):
                     eval("Modules." + module + ".execute(Modules." + module + ", content, file)")
             elif module not in modules['disabled']:
                 eval("Modules." + module + ".execute(Modules." + module + ", content, file)")
-
-
-# Scope all .php files in the given path
-def scope_files(path):
-    files = []
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.php' in file:
-                files.append(os.path.join(r, file))
-
-    return files
 
 
 # Read file and return It's content
